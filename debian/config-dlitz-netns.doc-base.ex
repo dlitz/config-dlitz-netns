6f6ac56ef1893a17e3ea75a6f44fbf9b5bd12102
Darsey Litzenberger <dlitz@dlitz.net>
2023-11-18 14:31:13 -0700

    Initial blank Debian package template

---
+++ debian/config-dlitz-netns.doc-base.ex
@@ -0,0 +1,20 @@
+Document: config-dlitz-netns
+Title: Debian config-dlitz-netns Manual
+Author: <insert document author here>
+Abstract: This manual describes what config-dlitz-netns is
+ and how it can be used to
+ manage online manuals on Debian systems.
+Section: unknown
+
+Format: debiandoc-sgml
+Files: /usr/share/doc/config-dlitz-netns/config-dlitz-netns.sgml.gz
+
+Format: postscript
+Files: /usr/share/doc/config-dlitz-netns/config-dlitz-netns.ps.gz
+
+Format: text
+Files: /usr/share/doc/config-dlitz-netns/config-dlitz-netns.text.gz
+
+Format: HTML
+Index: /usr/share/doc/config-dlitz-netns/html/index.html
+Files: /usr/share/doc/config-dlitz-netns/html/*.html
